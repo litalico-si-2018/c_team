05cd547d3c64507983ff98d6d1b1680bd1ae27eb
Masumi.M <masumi.lacrosse@live.jp>
2018-08-11 23:28:05 +0900

    Start ver

---
+++ Masumi.c/Assets/Hyouji_fade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hyouji_fade : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void hyouji_fade(){
+		gameObject.SetActive (true);
+		Invoke("DelayMethod", 1f);
+	}
+
+	void DelayMethod(){
+		gameObject.SetActive (false);
+	}
+
+}
---
+++ Masumi.c/Assets/Susumu_4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Susumu_4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void susumu_4(){
+		transform.position += new Vector3 (0, -1.9f, 0);
+	}
+}
