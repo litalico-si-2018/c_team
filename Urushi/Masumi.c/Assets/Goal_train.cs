b835dc56eee816d09845a5cec6b8e5db6cf93b31
UruRino0721 <urushihara.rino@6thsense.co.jp>
2018-08-12 11:29:49 +0900

    add Goal

---
+++ Urushi/Masumi.c/Assets/Goal_train.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal_train : MonoBehaviour {
+
+    private int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position += new Vector3(0, 0, 1);
+        float scale_size = 0.1f;
+        int finish_count = 70;
+
+        if (count < finish_count)
+        {
+            transform.localScale += new Vector3(scale_size, scale_size, 0);
+            count += 1;
+        }
+	}
+}
